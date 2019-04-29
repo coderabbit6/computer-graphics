e556dc41beb74a53905393b9b39fae1bd92d9c48
Song <2416841692@qq.com>
2019-04-29 12:40:28 +0800

    viewport finish

--- Project5/Project5/viewport.cpp
+++ Project5/Project5/viewport.cpp
@@ -21,9 +21,9 @@ void mydisplay() {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	float cx = 170, cy = 170;
+	float cx = 220, cy = 220;
 	float H, W = 200, aspect = 1;
-	setViewport(100,240, 100, 240);
+	setViewport(100,340, 100, 340);
 	setWindow(0, 400, 0, 400);
 	//自动裁剪
 	for (int i = 0; i <= 1000; i++)
@@ -33,10 +33,13 @@ void mydisplay() {
 			glClearColor(1.0, 1.0, 1.0, 0.0);
 		    glColor3f(1.0, 1.0, 1.0);
 			glBegin(GL_LINE_STRIP);
-			for (float x = 0; x < 140.0; x++) {
+			for (float x = 0; x < 240.0; x++) {
 				glVertex2f(x, x);
 			}
-			for (float x = 0; x < 140.0; x++) {
+			for (float x = 0; x < 240.0; x++) {
+				glVertex2f(x, -x + 400);
+			}
+			for (float x = 0; x < 240.0; x++) {
 				glVertex2f(x, 100/(x+2));
 			}
 			glEnd();
@@ -46,15 +49,52 @@ void mydisplay() {
 			setWindow(cx-W,cx+W,cy-H,cy+H);
 			glColor3f(0.0, 0.0, 0.0);
 			glBegin(GL_LINE_STRIP);
-			for (float x = 0; x < 140.0; x++) {
+			for (float x = 0; x < 240.0; x++) {
 				glVertex2f(x, x);
 			}
-			for (float x = 0; x < 140.0; x++) {
+			for (float x = 0; x < 240.0; x++) {
+				glVertex2f(x, -x+400);
+			}
+			for (float x = 0; x < 240.0; x++) {
 				glVertex2f(x, 100 / (x+2));
 			}
 			glEnd();
 			glutSwapBuffers();
 		
+	}
+}
+
+void mydisplay1() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	int R = 20;
+	GLdouble t = pi / 3.0;
+	float cx = 220, cy = 220;
+	float H = 200, W = 200, aspect = 1;
+	setViewport(100, 340, 100, 340);
+	setWindow(cx - W, cx + W, cy - H, cy + H);
+	//自动裁剪
+	for (int k = 0; k <= 1000; k++)
+	{
+		Sleep(500);
+		glClearColor(1.0, 1.0, 1.0, 0.0);
+		glClear(GL_COLOR_BUFFER_BIT);
+		W *= 1.1;
+		H = W * aspect;
+		setWindow(cx - W, cx + W, cy - H, cy + H);
+		glColor3f(0.0, 0.0, 0.0);
+		glBegin(GL_LINE_STRIP);
+		for (int j = 1; j <= 10; j++) {
+			R += 20;
+			t += (pi / 4);
+			for (int i = 1; i <= 6; i++) {
+				glVertex2f(cx + R * cos(i * t), cy + R * sin(i * t));
+			}
+		}
+		glEnd();
+		glutSwapBuffers();
+
 	}
 }
 
@@ -65,15 +105,15 @@ void myInit(void) {
 	
 }
 
-void main(int argc, char** argv) {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-	glutInitWindowSize(640, 480);
-	glutInitWindowPosition(100, 150);
-	glutCreateWindow("the test");
-	glutDisplayFunc(mydisplay);
-	myInit();
-	//setWindow(-5.0, 5.0, -0.3, 1.0);
-	//setViewport(0, 640, 0, 480);
-	glutMainLoop();
-}
+//void main(int argc, char** argv) {
+//	glutInit(&argc, argv);
+//	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+//	glutInitWindowSize(640, 480);
+//	glutInitWindowPosition(100, 150);
+//	glutCreateWindow("the test");
+//	glutDisplayFunc(mydisplay1);
+//	myInit();
+//	//setWindow(-5.0, 5.0, -0.3, 1.0);
+//	//setViewport(0, 640, 0, 480);
+//	glutMainLoop();
+//}
